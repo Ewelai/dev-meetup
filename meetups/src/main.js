a6ad6c4a89061bf7ba24fb3a4c515cbb2ed6aca8
Ewelai <palevo.ewelai@gmail.com>
2019-10-05 13:50:28 +0300

    feat: add login submit and data

--- meetups/src/main.js
+++ meetups/src/main.js
@@ -2,19 +2,9 @@ import Vue from 'vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
-import VeeValidate, {
-  Validator
-} from 'vee-validate';
-import ru from 'vee-validate/dist/locale/en';
 import App from './App.vue';
 
-Validator.localize('ru', ru);
-Vue.use(VeeValidate, {
-  locale: 'ru',
-});
-
 Vue.config.productionTip = false;
-Vue.use(VeeValidate);
 
 new Vue({
   router,
--- meetups/src/views/login/Login.js
+++ meetups/src/views/login/Login.js
@@ -1,16 +1,30 @@
 export default {
-  data() {
-    return {
-      passportData: null,
-      name: null,
-      passportDate: null,
-    };
-  },
+  data: () => ({
+    valid: true,
+    name: '',
+    nameRules: [
+      v => !!v || 'Name is required',
+      v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+    ],
+    email: '',
+    emailRules: [
+      v => !!v || 'E-mail is required',
+      v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+    ],
+    checkbox: false,
+    checkboxRules: [
+      v => !!v || 'You must agree to continue!'
+    ]
+  }),
 
-  computed: {
-    // Проверяем, что каждое поле формы валидно
-    isFormValid() {
-      return Object.keys(this.fields).every(field => this.fields[field].valid);
+  methods: {
+    submit() {
+      console.log(this.name)
+      console.log(this.email)
+      if (this.$refs.form.validate()) {
+        this.snackbar = true;
+        this.$refs.form.reset();
+      }
     },
   },
-};
+}
